b4aa8393fc5e4b2b3a07cc4332d68df36435f247
assetsathidev1 <meher@skroll.ai>
2023-10-29 15:17:40 +0530

    fashion yolo

--- README.md
+++ README.md
@@ -2,5 +2,21 @@
 Repo to test out paperspace platforms
 
 
+## Paperspace Notes: 
+- Video+ Tutorial slightly older but still works. 
+- All deployments come with `/healthcheck`
+- Models to be Uploaded and path to be specified as `/opt/models/<model_file_name>`
+- TF, Onnx models supported out of box; also has custom option
+
 ## Paperspace Questions during setup:
-- 
+- Where did we provide the metadata for the [model](https://youtu.be/voyqmlYOIH0?feature=shared&t=266)
+
+- Check the image below this is slightly confusing. 
+![confusing ux](imgs/deploy_model_ux.png)
+
+- New UI changes breaks the old demo.
+    - `<endpoint>/v1/models/fashion-mnist/metadata` does not seem to work, tried my model name, model id and deployment name as well.
+    - Tried a `CLI` deployment with `cli-fashion-deply-spec.yaml` worked as expected on metadata and prediction
+    - Updated the `path:"/opt/mosqqhgydnbf3wu"` to `path:"/opt/models/fashion-mnist"` in `paperspace.json` to `modif_config_paperspace.json` and uploaded this new config in deployment and everything works again. 
+
+- Will disabling the deployment stop the billing? (i doubt it)
---
+++ test_fashion_yolos.py
@@ -0,0 +1,64 @@
+from PIL import Image
+from transformers import YolosFeatureExtractor, YolosForObjectDetection
+import torch
+import matplotlib.pyplot as plt
+
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+cats = ['shirt, blouse', 'top, t-shirt, sweatshirt', 'sweater', 'cardigan', 'jacket', 'vest', 'pants', 'shorts', 'skirt', 'coat', 'dress', 'jumpsuit', 'cape', 'glasses', 'hat', 'headband, head covering, hair accessory', 'tie', 'glove', 'watch', 'belt', 'leg warmer', 'tights, stockings', 'sock', 'shoe', 'bag, wallet', 'scarf', 'umbrella', 'hood', 'collar', 'lapel', 'epaulette', 'sleeve', 'pocket', 'neckline', 'buckle', 'zipper', 'applique', 'bead', 'bow', 'flower', 'fringe', 'ribbon', 'rivet', 'ruffle', 'sequin', 'tassel']
+
+def idx_to_text(i):
+    return cats[i]
+
+# for output bounding box post-processing
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+def visualize_predictions(image, outputs, threshold=0.8):
+    # keep only predictions with confidence >= threshold
+    probas = outputs.logits.softmax(-1)[0, :, :-1]
+    keep = probas.max(-1).values > threshold
+
+    # convert predicted boxes from [0; 1] to image scales
+    bboxes_scaled = rescale_bboxes(outputs.pred_boxes[0, keep].cpu(), image.size)
+
+    # plot results
+    plot_results(image, probas[keep], bboxes_scaled)
+    return bboxes_scaled
+
+def plot_results(pil_img, prob, boxes):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    colors = COLORS * 100
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        ax.text(xmin, ymin, idx_to_text(cl), fontsize=10,
+                bbox=dict(facecolor=c, alpha=0.8))
+    plt.axis('off')
+    plt.savefig("outputs/image.png")
+
+MODEL_NAME = "valentinafeve/yolos-fashionpedia"
+FEATURE_EXTRACTOR_NAME = "hustvl/yolos-small"
+feature_extractor = YolosFeatureExtractor.from_pretrained(FEATURE_EXTRACTOR_NAME)
+model = YolosForObjectDetection.from_pretrained(MODEL_NAME)
+
+
+path = "test_imgs/t1.jpg"
+image = Image.open(path)
+
+inputs = feature_extractor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+res = visualize_predictions(image, outputs, threshold=0.5)
